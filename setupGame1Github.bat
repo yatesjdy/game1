7c676b7010599f46c0632d54042158c11030f619
Jeff <yatesjdy@Jeffreys-MacBook-Air.local>
2017-03-07 10:26:36 -0800

    cmd line commit of game1 folder contents

---
+++ setupGame1Github.bat
@@ -0,0 +1,8 @@
+echo "# game1" >> README.md
+git config --global user.name yatesjdy
+git config --global user.email yatesjdy@comcast.net
+/usr/bin/git init
+/usr/bin/git add README.md
+/usr/bin/git commit -m "first commit"
+/usr/bin/git remote add origin https://github.com/modocgames/game1.git
+/usr/bin/git push -u origin master
